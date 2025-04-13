7ba571c48a3676eacd0aa6a5d03058a116453b2a
Victor-201 <4.victor.201@gmail.com>
2025-04-13 16:08:54 +0700

    delete: delete all project

--- model_file_not_found_error_handler.py
+++
@@ -1,3 +0,0 @@
-MODEL_PATH = "shape_predictor_68_face_landmarks.dat"
-if not os.path.exists(MODEL_PATH):
-    raise FileNotFoundError(f"Không tìm thấy mô hình: {MODEL_PATH}")
--- shape_predictor_model_path.py
+++
@@ -1,1 +0,0 @@
-MODEL_PATH = "/Users/nguyenhai/PyCharmMiscProject/shape_predictor_68_face_landmarks.dat"
